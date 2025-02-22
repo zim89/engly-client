d4253cbba27e5b4c54d3227c1ebba4c3871e2e30
FernirTeam <121241841+zim89@users.noreply.github.com>
2025-02-22 15:53:59 +0200

    fixed deploy errors

--- src/features/auth/lib/validators.ts
+++ src/features/auth/lib/validators.ts
@@ -1,9 +1,7 @@
 import { type UseFormReturn } from 'react-hook-form'
-import { type RegisterFormData } from '../model'
+import type { RegisterFormValues } from './register-schema'
 
-export const validatePasswords = (
-  form: UseFormReturn<RegisterFormData>,
-): boolean => {
+export const validatePasswords = (form: UseFormReturn<RegisterFormValues>): boolean => {
   const { password, confirm } = form.getValues()
 
   if (!password || !confirm) {
